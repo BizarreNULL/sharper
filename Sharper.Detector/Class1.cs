6743db8eaa7b4555850d633c6cb92d448129a0ed
Jojo <jonas.uliana@passwd.com.br>
2021-02-16 17:19:10 -0300

    feat: added .NET binaries detection with assembly reflection

--- Sharper.Detector/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Sharper.Detector
-{
-    public class Class1
-    {
-    }
-}
---
+++ Sharper.Detector/Fingerprint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace Sharper.Detector
+{
+    /// <summary>
+    /// Show functionalities to check if given file is a .NET binary.
+    /// </summary>
+    public static class Fingerprint
+    {
+        /// <summary>
+        /// Check if given file have correct image signature for a .NET binary.
+        /// </summary>
+        /// <param name="fullPath">Complete path to file to be tested.</param>
+        /// <returns>If the binary is a valid .NET one.</returns>
+        public static bool IsDotNetBinary(string fullPath)
+        {
+            try
+            {
+                // Checks more deeper than just unmanaged binaries magic bytes 4d 5a 90 00 03.  
+                _ = AssemblyName.GetAssemblyName(fullPath ?? throw new ArgumentNullException(nameof(fullPath)));
+                return true;
+            }
+            catch (BadImageFormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
