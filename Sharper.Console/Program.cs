0b6e73d0c1f7093f2c6d431bb5df67cd7cc78027
Jojo <jonas.uliana@passwd.com.br>
2021-02-16 14:10:19 -0300

    chore: added Sharper.Console as console project

---
+++ Sharper.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sharper.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
